4def7451e105309fff7f5f98f54cc3492ddcfa5b
ahhz <a.hagen-zanker@surrey.ac.uk>
2018-01-05 13:34:35 +0000

    add tests

---
+++ tests/edge_view_tests.cpp
@@ -0,0 +1,79 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#include <gtest/gtest.h>
+
+#include <blink/raster/filesystem.h>
+#include <blink/raster/io.h>
+#include <blink/raster/gdal_raster_view.h>
+#include <blink/raster/edge_raster.h>
+
+#include <vector>
+
+namespace br = blink::raster;
+namespace fs = br::filesystem;
+
+bool test_v_edge()
+{
+  auto r = br::create_temp<int>(3, 5);
+  int count = 0;
+  for (auto&& i : r) {
+    i = count++;
+  }
+
+  auto t = br::v_edge(r);
+  
+  std::vector < std::pair<int, int> > check_vector;
+  for (auto&& i : t) {
+    check_vector.push_back(i);
+  }
+  std::vector < std::pair<int, int> > c2;
+  c2.emplace_back(0,1);
+  c2.emplace_back(1,2);
+  c2.emplace_back(2,3);
+  c2.emplace_back(3, 4);
+  c2.emplace_back(5, 6);
+  c2.emplace_back(6, 7);
+  c2.emplace_back(7, 8);
+  c2.emplace_back(8, 9);
+  c2.emplace_back(10, 11);
+  c2.emplace_back(11, 12);
+  c2.emplace_back(12, 13);
+  c2.emplace_back(13, 14);
+  
+  return c2==check_vector;
+}
+
+
+bool test_h_edge()
+{
+  auto r = br::create_temp<int>(3, 5);
+  int count = 0;
+  for (auto&& i : r) {
+    i = count++;
+  }
+
+  auto t = br::h_edge(r);
+
+  std::vector < std::pair<int, int> > check_vector;
+  for (auto&& i : t) {
+    check_vector.push_back(i);
+  }
+  std::vector < std::pair<int, int> > c2;
+  c2.emplace_back(0,5);
+  c2.emplace_back(1, 6);
+  c2.emplace_back(2, 7);
+  c2.emplace_back(3, 8);
+  c2.emplace_back(4, 9);
+  c2.emplace_back(5, 10);
+  c2.emplace_back(6, 11);
+  c2.emplace_back(7, 12);
+  c2.emplace_back(8, 13);
+  c2.emplace_back(9, 14);
+
+  return c2 == check_vector;
+}
+
+TEST(RasterTest, EdgeRaster) {
+  EXPECT_TRUE(test_v_edge());
+  EXPECT_TRUE(test_h_edge());
+ }
+
---
+++ tests/io_tests.cpp
@@ -0,0 +1,105 @@
+#define _SILENCE_TR1_NAMESPACE_DEPRECATION_WARNING
+#include <gtest/gtest.h>
+
+#include <blink/raster/filesystem.h>
+#include <blink/raster/io.h>
+#include <blink/raster/gdal_raster_view.h>
+
+#include <vector>
+
+namespace br = blink::raster;
+namespace fs = br::filesystem;
+
+bool test_create_temp()
+{
+  auto r = br::create_temp<int>(5, 3);
+  int count = 0;
+  for (auto&& i : r) {
+    i = count++;
+  }
+  std::vector<int> check_vector;
+  for (auto&& i : r) {
+    check_vector.push_back(i);
+  }
+  return check_vector == std::vector<int>
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+}
+
+bool test_create()
+{
+  {
+    auto r = br::create<int>("temp.tif", 5, 3);
+    int count = 0;
+    for (auto&& i : r) {
+      i = count++;
+    }
+  } // leave scope
+  bool check_exist = fs::exists("temp.tif");
+  fs::remove("temp.tif");
+  bool check_not_exist = !fs::exists("temp.tif");
+  return check_exist && check_not_exist;
+}
+
+bool test_open()
+{
+  {
+    auto r = br::create<int>("temp.tif", 5, 3);
+    int count = 0;
+    for (auto&& i : r) {
+      i = count++;
+    }
+  } // leave scope
+  bool check_exist = fs::exists("temp.tif");
+  bool check_contents;
+  {
+    auto r = blink::raster::open<int>("temp.tif", br::access::read_only);
+    std::vector<int> check_vector;
+    for (auto&& i : r) {
+      check_vector.push_back(i);
+    }
+    check_contents =  check_vector == std::vector<int>
+     {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
+  }
+  fs::remove("temp.tif");
+  bool check_not_exist = !fs::exists("temp.tif");
+  return check_exist && check_not_exist && check_contents;
+}
+
+bool test_create_open_large()
+{
+  int rows = 2000;
+  int cols = 1500;
+  {
+    auto view = br::create<int>("temp.tif", rows, cols);
+    int count = 0;
+    for (auto&& i : view) {
+      i = count++;
+    }
+  }
+  bool check_exist = fs::exists("temp.tif");
+
+  bool check_contents;
+  {
+    auto view = br::open<int>("temp.tif", br::access::read_only);
+
+    std::vector<int> check_vector;
+    for (auto&& i : view) {
+      check_vector.push_back(i);
+    }
+    int check_last = check_vector.back() == (rows * cols - 1);
+    check_vector.resize(15);
+    check_contents = check_vector == std::vector<int>
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14}
+    && check_last;
+  }
+  fs::remove("temp.tif");
+  bool check_not_exist = !fs::exists("temp.tif");
+  return check_exist && check_not_exist && check_contents;
+}
+
+TEST(RasterTest, IO) {
+  EXPECT_TRUE(test_create_temp());
+  EXPECT_TRUE(test_create());
+  EXPECT_TRUE(test_open());
+  EXPECT_TRUE(test_create_open_large());
+}
