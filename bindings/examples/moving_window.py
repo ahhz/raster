7443e21ee7919085e44b4585f65dca974ed23722
Alex Hagen-Zanker <a.hagen-zanker@surrey.ac.uk>
2021-07-02 11:59:11 +0100

    Update moving_window.py

--- bindings/examples/moving_window.py
+++ bindings/examples/moving_window.py
@@ -2,9 +2,9 @@
 import pronto_python
 pronto_python.moving_window_pattern( "input.tif", "output.tif"
                               , 3.0
-                              , example.Metric.AreaWeightedPatchSize
-                              , example.WindowShape.Square)
+                              , pronto_python.Metric.AreaWeightedPatchSize
+                              , pronto_python.WindowShape.Square)
 pronto_python.moving_window_pattern( "input.tif", "output2.tif"
                               , 3.0
-                              , example.Metric.EdgeDensity
-                              , example.WindowShape.Square)
+                              , pronto_python.Metric.EdgeDensity
+                              , pronto_python.WindowShape.Square)
