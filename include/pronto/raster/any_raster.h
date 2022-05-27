9df00cfbf5e06556494c03e95bca29af0c3fa524
Alex Hagen-Zanker <a.hagen-zanker@surrey.ac.uk>
2022-05-27 13:34:14 +0100

    create type-erased_raster will need to replace any_raster at a later point

--- include/pronto/raster/any_raster.h
+++ include/pronto/raster/any_raster.h
@@ -10,16 +10,264 @@
 
 #pragma once
 
+#include <pronto/raster/access_type.h>
+#include <pronto/raster/iterator_facade.h>
 #include <pronto/raster/reference_proxy.h>
 #include <pronto/raster/traits.h>
 
 #include <any>
 #include <cassert>
+#include <functional>
 #include <type_traits>
 
 namespace pronto {
   namespace raster {
 
+    //Derived must have operator T() and operator=(const T&) member functions
+    template<typename Derived, typename T>
+    class proxy_reference
+    {
+      using self_type = Derived;
+      using value_type = T;
+
+    public:
+      proxy_reference() = default;
+      proxy_reference(const proxy_reference&) = default;
+      proxy_reference(proxy_reference&&) = default;
+      
+      void put(const value_type& v) const {
+        _self() = v;
+      }
+
+      value_type get() const {
+        return static_cast<value_type>(_self());
+      }
+
+      const self_type& operator++() const
+      {
+        auto temp = get();
+        put(++temp);
+        return _self();
+      }
+      const self_type& operator--() const
+      {
+        put(--get());
+        return _self();
+      }
+
+      value_type operator++(int) const
+      {
+        value_type temp = get();
+        put(++get());
+        return temp;
+      }
+      value_type operator--(int) const
+      {
+        value_type temp = get();
+        put(--get());
+        return temp;
+      }
+
+    private:
+      self_type& _self() {
+        return static_cast<self_type&>(*this);
+      }
+
+      const self_type& _self() const {
+        return static_cast<const self_type&>(*this);
+      }
+    public:
+
+      // MACRO to implement assigning operator
+#define PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(op)      \
+template<class U> const self_type& operator op(const U& v) const \
+{ auto temp = _self().operator value_type(); temp op v;_self().operator=(temp); return _self(); }
+
+      // All assigning operators.
+      // For the sake of some compilers deeclared after m_accessors
+      PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(+= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(-= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(/= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(*= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(%= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(&= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(|= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(^= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(<<= )
+        PRONTO_RASTER_PROXY_REFERENCE_ASSIGNING_OPERATOR(>>= )
+    };
+
+
+    template<class T, access AccessType = access::read_write>
+    class type_erased_reference : public proxy_reference<type_erased_reference<T,AccessType>, T>
+    {
+      static const bool is_mutable = AccessType != access::read_only;
+
+    public:
+      type_erased_reference(const type_erased_reference&) = default;
+      type_erased_reference(type_erased_reference&&) = default;
+      ~type_erased_reference() = default;
+      type_erased_reference& operator=(const type_erased_reference& other)
+      {
+        m_put(other.m_get());
+        return *this;
+      }
+      type_erased_reference& operator=(type_erased_reference&& other)
+      {
+        m_put(other.m_get());
+        return *this;
+
+      }
+      type_erased_reference(T& r)
+       {
+         auto ref = std::ref(r);
+         m_get = [ref]() {return ref.get(); };
+         
+         if constexpr (is_mutable) {
+           m_put = [ref](const T& v) {ref.get() = v; };
+         }
+         else {
+           m_put = [](const T&) {assert(false); };
+         }
+       }
+
+       type_erased_reference(const T& r)
+       {
+         auto ref = std::ref(r);
+         m_get = [ref]() {return ref.get(); };
+         m_put = [](const T&) {assert(false); };
+       }
+
+       template<class Proxy>
+       type_erased_reference(const Proxy& p)
+       {
+         m_get = [p]() {return static_cast<T>(p); };
+         if constexpr (is_mutable) {
+           m_put = [p](const T& v) {p = v; };
+         }
+         else {
+           m_put = [](const T&) {assert(false); };
+         }
+       }
+        operator T() const 
+        {
+          return m_get();
+        }
+
+       const type_erased_reference& operator=(const T& value) const
+         requires is_mutable
+       {
+         m_put(value);
+         return *this;
+       }
+
+       std::function<T()> m_get;
+       std::function<void(T)> m_put;
+    };
+
+    template<class T, class IterationType = multi_pass, access AccessType = access::read_write>
+    class type_erased_raster_iterator : public iterator_facade<type_erased_raster_iterator<T> >
+    {
+
+    public:
+      static const bool is_mutable = AccessType != access::read_only;
+      static const bool is_single_pass = std::is_same_v<IterationType, single_pass>;
+      type_erased_raster_iterator() = default;
+      type_erased_raster_iterator(const  type_erased_raster_iterator& iter) = default;
+      type_erased_raster_iterator(type_erased_raster_iterator&& iter) = default;
+      type_erased_raster_iterator& operator=(const type_erased_raster_iterator&) = default;
+      type_erased_raster_iterator& operator=(type_erased_raster_iterator&&) = default;
+      ~type_erased_raster_iterator() = default;
+
+      template<class Iter>
+       type_erased_raster_iterator(const Iter& iter)
+      {
+        using iter_type = std::remove_cv_t<Iter>;
+        m_any_iter = iter;
+        m_increment = [&]() {++std::any_cast<iter_type&>(m_any_iter); };
+        m_decrement = [&]() {--std::any_cast<iter_type&>(m_any_iter); };
+        m_advance = [&](std::ptrdiff_t offset) {std::any_cast<iter_type&>(m_any_iter) += offset; } ;
+        m_equal_to = [&](const type_erased_raster_iterator& other)
+        {
+          return std::any_cast<iter_type&>(m_any_iter) == std::any_cast<const iter_type&>(other.m_any_iter); 
+        };
+        
+        m_distance_to = [&](const type_erased_raster_iterator& other)
+        { 
+          return std::any_cast<const iter_type&>(other.m_any_iter) - std::any_cast<iter_type&>(m_any_iter); 
+        };
+        
+        m_dereference = [&]() {return type_erased_reference<T>(*(std::any_cast<iter_type&>(m_any_iter))); };
+
+      }
+
+       type_erased_reference<T, AccessType> dereference() const { return m_dereference(); };
+       void increment() { m_increment(); }
+       void decrement() { m_decrement(); }
+       void advance(std::ptrdiff_t offset) { m_advance(offset); }
+       bool equal_to(const type_erased_raster_iterator& other) const { return m_equal_to(other); };
+       std::ptrdiff_t distance_to(const type_erased_raster_iterator& other) const { return m_distance_to(other); };
+
+    private:
+      std::function<type_erased_reference<T,AccessType>()> m_dereference;
+      std::function<void()> m_increment;
+      std::function<void()> m_decrement;
+      std::function<void(std::ptrdiff_t )> m_advance;
+      std::function<bool(const type_erased_raster_iterator& )> m_equal_to;
+      std::function<std::ptrdiff_t(const type_erased_raster_iterator& )> m_distance_to;
+     
+      std::any m_any_iter;
+    };
+
+    template<class T, class IterationType = multi_pass, access AccessType = access::read_write>
+    class type_erased_raster : public std::ranges::view_interface<type_erased_raster<T, IterationType, AccessType> >
+    {
+    public:
+      static const bool is_mutable = AccessType != access::read_only;
+      static const bool is_single_pass = std::is_same_v<IterationType, single_pass>;
+
+      using iterator = typename type_erased_raster_iterator<T, IterationType, AccessType>;
+
+      type_erased_raster() {};
+      ~type_erased_raster() {};
+      template<class Raster>
+      type_erased_raster(const Raster& r) {
+        m_raster = r;
+        using raster_type= std::remove_cv_t<Raster>;
+        m_begin = [&]() {return iterator( std::any_cast<raster_type&>(m_raster).begin() ); };
+        m_end =   [&]() {return iterator(std::any_cast<raster_type&>(m_raster).end()); };
+        m_rows =  [&]() {return std::any_cast<raster_type&>(m_raster).rows(); };
+        m_cols =  [&]() {return std::any_cast<raster_type&>(m_raster).cols(); };
+        m_size =  [&]() {return std::any_cast<raster_type&>(m_raster).size(); };
+        m_sub_raster = [&](int a, int b, int c, int d)
+        {
+          return type_erased_raster<T>(std::any_cast<raster_type&>(m_raster).sub_raster(a,b,c,d) );
+        };
+      };
+
+      iterator begin() const { return m_begin(); }
+      iterator end()   const { return m_end(); }
+      int rows()       const { return m_rows(); }
+      int cols()       const { return m_cols(); }
+      int size()       const { return m_size(); }
+      type_erased_raster sub_raster(int a, int b, int c, int d) const { return m_sub_raster(a, b, c, d); }
+   
+    private:
+      std::function<iterator()> m_begin;
+      std::function<iterator()> m_end;
+      std::function<int()> m_rows;
+      std::function<int()> m_cols;
+      std::function<int()> m_size;
+      std::function <type_erased_raster(int, int, int, int)> m_sub_raster;
+
+      std::any m_raster;
+
+    };
+
+   // template<class T, class IterationType = multi_pass, access AccessType = access::read_write>
+   // using any_raster = type_erased_raster<T, IterationType, AccessType>;
+
+    
     template<class T> class any_raster;
     template<class T> class any_raster_iterator;
     template<class T> class any_raster_const_iterator;
@@ -278,6 +526,7 @@ namespace pronto {
 
       std::any m_holding;
     };
+    
 
     template<class Raster>
     any_raster<typename traits<Raster>::value_type>
--- tests/any_blind_raster_tests.cpp
+++ tests/any_blind_raster_tests.cpp
@@ -15,10 +15,109 @@
 #include <pronto/raster/blind_function.h>
 #include <pronto/raster/io.h>
 
+#include <ranges>
 
 namespace pr = pronto::raster;
 
+class prox : public pr::proxy_reference<prox,int>
+{
+public:
 
+	prox(std::vector<int>& v) : vec{v}, target(3)
+	{}
+	
+	prox(const prox& p) : target{p.target}, vec(p.vec)
+	{}
+
+	operator int() const
+	{
+		return vec[target];
+	}
+	
+	const prox& operator=(const int& v) const {
+		vec[target] = v;
+		return *this;
+	}
+
+	void set_target(int t)
+	{
+		target = t;
+	}
+	int target = 3;
+	std::vector<int>& vec;
+};
+
+bool test_type_erased_reference()
+{
+	std::vector<int> v{ 1,2,3,4,5 };
+	prox p{ v };
+	p = 1;
+	p.set_target(2);
+	pr::type_erased_reference<int>  pe(p);
+	++pe;
+	pe+=3;
+
+	int x = 100;
+	int& xref = x;
+	xref += 100;
+	pr::type_erased_reference<int> xref_erased(xref);
+	xref_erased /= 4;
+
+	return v == std::vector{ 1, 2, 7, 1, 5 } && x == 50;
+	
+}
+
+bool test_type_erased_raster()
+{
+	int rows = 2;
+	int cols = 3;
+	auto a = pr::create_temp<int>(rows, cols);
+	int v = 0;
+	for (auto&& i : a) {
+		v += 1;
+	  i = v;
+	}
+
+	pr::type_erased_raster_iterator<int> iter(a.begin());
+	pr::type_erased_raster_iterator<int> end(a.end());
+	static_assert(std::input_iterator<decltype(iter)>);
+	static_assert(std::derived_from < std::_Iter_concept<decltype(iter)>, std::forward_iterator_tag>);
+	static_assert(!decltype(iter)::is_single_pass);
+	static_assert(std::default_initializable<decltype(iter)>);
+
+	static_assert(std::incrementable<decltype(iter)>);
+
+	static_assert(std::forward_iterator<decltype(iter)>);
+	int w = 1;
+	for(;iter!=end;++iter){
+		w *= 10;
+		*iter *=  w;
+	}
+	std::vector<int> vec; 
+
+	pr::type_erased_raster<int> a_erase(a);
+	static_assert(std::ranges::range<decltype(a)>);
+	static_assert(std::ranges::view<decltype(a)>);
+	static_assert(std::ranges::random_access_range<decltype(a)>);
+	static_assert(std::ranges::sized_range<decltype(a)>);
+	static_assert(!std::ranges::contiguous_range<decltype(a)>);
+	
+	;
+
+	static_assert(std::ranges::range<decltype(a_erase)>);
+	static_assert(std::ranges::view<decltype(a_erase)>);
+	static_assert(std::ranges::random_access_range<decltype(a_erase)>);
+	static_assert(std::ranges::sized_range<decltype(a_erase)>);
+	static_assert(!std::ranges::contiguous_range<decltype(a_erase)>);
+
+
+	for (auto&& i : a_erase) {
+		vec.push_back(i);
+	}
+	
+	return vec == std::vector<int>{ 10, 200, 3000, 40000, 500000,6000000 };
+
+}
 bool test_get_any_blind_raster()
 {
 	int rows = 3;
@@ -38,6 +137,8 @@ bool test_get_any_blind_raster()
 
 
 TEST(RasterTest, AnyBlindRaster) {
-  EXPECT_TRUE(test_get_any_blind_raster());
+	EXPECT_TRUE(test_type_erased_reference());
+	EXPECT_TRUE(test_type_erased_raster());
+	EXPECT_TRUE(test_get_any_blind_raster());
 }
 
